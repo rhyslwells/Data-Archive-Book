7cf4cdfcbfd6b3ebba126a09e0784343b614a956
Rhys Wells <rhyslwells@outlook.com>
2025-04-21 07:49:36 +0100

    katex works now

---
+++ .history/DAB/export-to-pdf_20250421074206.js
@@ -0,0 +1,25 @@
+// export-to-pdf.js
+const puppeteer = require('puppeteer');
+const path = require('path');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  const indexPath = 'file://' + path.resolve(__dirname, '_book/index.html');
+  await page.goto(indexPath, { waitUntil: 'networkidle0' });
+
+  await page.pdf({
+    path: 'mybook.pdf',
+    format: 'A4',
+    printBackground: true,
+    margin: {
+      top: '20mm',
+      bottom: '20mm',
+      left: '15mm',
+      right: '15mm'
+    }
+  });
+
+  await browser.close();
+})();
